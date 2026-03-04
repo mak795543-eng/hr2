84e37342285864908ff4394cc8477b1359934143
badematthewlouisdiaz <badematthewlouisdiaz@gmail.com>
2026-03-04 20:45:17 +0800

    g

--- USM/sidebarr.php
+++ USM/sidebarr.php
@@ -272,7 +272,7 @@ if (!function_exists('sidebarActiveClass')) {
                         <div class="p-1 rounded-lg bg-blue-800/30 group-hover:bg-blue-700/50 transition-colors">
                             <i data-lucide="send" class="w-5 h-5 text-[#F7B32B] group-hover:text-white"></i>
                         </div>
-                        <span class="ml-3 sidebar-text">Department Request Logs</span>
+
                     </div>
                 </a>
             <?php endif; ?>
