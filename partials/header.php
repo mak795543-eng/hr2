04d671771b9dd2e63654a3808019411715a47fb2
badematthewlouisdiaz <badematthewlouisdiaz@gmail.com>
2026-03-04 21:32:18 +0800

    gggggg

--- partials/header.php
+++ partials/header.php
@@ -12,7 +12,7 @@ if (!function_exists('dd')) {
 }
 
 if (!isset($_SESSION['employee_id'])) {
-    header('Location: /hr2/index.php');
+    header('Location: /index.php');
     exit;
 }
 
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
         );
     }
     session_destroy();
-    header('Location: /hr2/index.php');
+    header('Location: /index.php');
     exit;
 }
 ?>
